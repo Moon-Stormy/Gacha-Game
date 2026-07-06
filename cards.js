9c32f16114e5fff19aaf3656f49d9a9d550d7646
Moon-Stormy <sayfploquin@gmail.com>
2026-07-06 16:08:09 +0200

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,18 +1,25 @@
 const cards = [
-    { name: "Puff", file: "puff.png", rarity: "rare" },
+    // Common
     { name: "Souf", file: "souf.png", rarity: "common" },
-    { name: "Le Royaume de Nelle", file: "lesenfersdenelle.png", rarity: "legendary" },
     { name: "Ministance", file: "ministance.png", rarity: "common" },
-    { name: "Tom le Goat du hand", file: "tomhandball.png", rarity: "legendary" },
-    { name: "Eliott Gamer", file: "eliottgaming.png", rarity: "epic" },
-    { name: "Dresseur Jules", file: "pokejules.png", rarity: "epic" },
-    { name: "Trésor du congélo", file: "absolutvodka.png", rarity: "rare" },
-    { name: "Ambre au pays des bisounours", file: "amber.png", rarity: "rare" },
     { name: "Cubeliott", file: "cubeliott.png", rarity: "common" },
     { name: "Hannarabe", file: "anna.png", rarity: "common" },
     { name: "Tominou", file: "tominou.png", rarity: "common" },
     { name: "Nel & co", file: "nel.png", rarity: "common" },
     { name: "Positive Alizouille", file: "alizouille.png", rarity: "common" },
     { name: "Jul (pas le rappeur)", file: "jul.png", rarity: "common" },
-    { name: "Maxime found Gem", file: "maximegem.png", rarity: "epic" }
-]; 
+
+    // Rare
+    { name: "Puff", file: "puff.png", rarity: "rare" },
+    { name: "Trésor du congélo", file: "absolutvodka.png", rarity: "rare" },
+    { name: "Ambre au pays des bisounours", file: "amber.png", rarity: "rare" },
+
+    // Epic
+    { name: "Eliott Gamer", file: "eliottgaming.png", rarity: "epic" },
+    { name: "Dresseur Jules", file: "pokejules.png", rarity: "epic" },
+    { name: "Maxime found Gem", file: "maximegem.png", rarity: "epic" },
+
+    // Legendary
+    { name: "Le Royaume de Nelle", file: "lesenfersdenelle.png", rarity: "legendary" },
+    { name: "Tom le Goat du hand", file: "tomhandball.png", rarity: "legendary" }
+];
